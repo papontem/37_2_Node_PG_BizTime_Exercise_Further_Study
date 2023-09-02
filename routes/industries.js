c19a75b84a3596a015b33c44445a628092ba1613
papontemjetix <papontem@gmail.com>
2023-09-01 20:49:55 -0400

    preogress

--- routes/industries.js
+++ routes/industries.js
@@ -9,14 +9,36 @@ const db = require("../db");
 // which should show the company code(s) for that industry
 router.get("/", async (req, res, next) => {
 	try {
-		const results = await db.query(`
+		let code_list = [];
+		const industriesArray = [];
+		const query_res = await db.query(`
         SELECT i.code ,i.field , c_i.company_code AS comp_code
         FROM industries AS i
         LEFT JOIN company_industries AS c_i
-            ON i.code = c_i.industry_code
+        ON i.code = c_i.industry_code
         `);
-		
-		return res.json({ industries: results.rows });
+
+		// lets destructure each row one by one
+		for (let i = 0; i < query_res.rows.length; i++) {
+			const row = query_res.rows[i];
+			const { code, field, comp_code } = row;
+			console.log("Current code: ", code);
+			console.log("Current field: ", field);
+			console.log("Current comp_code: ", comp_code);
+
+			if (!code_list.includes(code)) {
+				console.log("NEW CODE!!!");
+				code_list.push(code);
+			} else {
+				console.log("OLD CODE!!!");
+				// find the index for the first code, that will
+                // be theindexof the industry we
+                // need to append a new comp_code to
+                
+			}
+			console.log("code_list:", code_list);
+		}
+		return res.json({ industries: query_res.rows });
 	} catch (e) {
 		return next(e);
 	}
