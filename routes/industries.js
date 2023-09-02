0cde1898eabb769bd9d9c678a9700f19fb5df6ca
papontemjetix <papontem@gmail.com>
2023-09-01 20:59:15 -0400

    forgot about javascripts Aray.findIndex() method, got the results i wanted much quicker using that methed.

--- routes/industries.js
+++ routes/industries.js
@@ -22,23 +22,41 @@ router.get("/", async (req, res, next) => {
 		for (let i = 0; i < query_res.rows.length; i++) {
 			const row = query_res.rows[i];
 			const { code, field, comp_code } = row;
-			console.log("Current code: ", code);
-			console.log("Current field: ", field);
-			console.log("Current comp_code: ", comp_code);
+			// console.log("Current code: ", code);
+			// console.log("Current field: ", field);
+			// console.log("Current comp_code: ", comp_code);
 
 			if (!code_list.includes(code)) {
-				console.log("NEW CODE!!!");
+				// console.log("NEW CODE!!!");
 				code_list.push(code);
+				industriesArray.push({
+					code,
+					field,
+					companies: [comp_code],
+				});
 			} else {
-				console.log("OLD CODE!!!");
+				// console.log("OLD CODE!!!");
 				// find the index for the first code, that will
-                // be theindexof the industry we
-                // need to append a new comp_code to
-                
+				// be the index of the industry we
+				// need to append a new comp_code to
+
+				// const array1 = [5, 12, 8, 130, 44];
+				// const isLargeNumber = (element) => element > 13;
+				// console.log(array1.findIndex(isLargeNumber));
+				// // Expected output: 3
+
+				let industry_Array_Index = code_list.findIndex(
+					(code_element) => code_element === code
+				);
+				console.log("industry_Array_Index:", industry_Array_Index);
+                industriesArray[industry_Array_Index].companies.push(comp_code)
 			}
-			console.log("code_list:", code_list);
+            
 		}
-		return res.json({ industries: query_res.rows });
+        // console.log("code_list:", code_list);
+        // console.log("Industries Array:", industriesArray);
+        
+		return res.json({ industries: industriesArray });
 	} catch (e) {
 		return next(e);
 	}
